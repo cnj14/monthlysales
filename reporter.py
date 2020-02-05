de561c47ba1a6000786e2003b66b80d419f3fd41
cnj14 <59627768+cnj14@users.noreply.github.com>
2020-02-05 17:52:44 -0500

    Reading p1

--- reporter.py
+++ reporter.py
@@ -1,3 +1,6 @@
+import pandas
+
+
 def to_usd(my_price):
     """
     Converts a numeric value to usd-formatted string, for printing and display purposes.
@@ -8,4 +11,19 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
-print("GENERATING SALES REPORT FOR MONTH OF OCTOBER 2013...")
+# print("GENERATING SALES REPORT FOR MONTH OF OCTOBER 2013...")
+
+print("READING GRADEBOOK CSV FILE...")
+
+# if CSV file in same directory as this filepath script:
+csv_filepath = "gradebook.csv"
+grades = pandas.read_csv(csv_filepath)
+print("GRADES:", type(grades))
+print(grades.head())
+
+
+
+
+
+# csv_filepath = "/path/to/jeter_stats.csv"
+#stats = pandas.read_csv(csv_filepath)
